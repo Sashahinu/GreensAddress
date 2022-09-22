c9863a3c8bc3aef23f1358b2904a0c1566402ad0
safashabeer <safashabeer@192.168.29.247>
2022-09-22 10:25:35 +0530

    Commit to SafaBranch

---
+++ src/org/add/GreensTech2.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech2 {
+	private void greensOmr() {
+		System.out.println("Address is Omr");	
+	}
+	
+	private void greensAdyar() {
+		System.out.println("Address is Adayar");	
+	}
+	
+	private void greensTambaram() {
+		System.out.println("Address is Tambaram");
+		
+	}
+	
+	private void greensVelachery() {
+		System.out.println("Address is Velachey");
+		
+	}
+	
+	private void greensAnnaNagar() {
+		System.out.println("Address is AnnaNagar");
+		
+	}
+	
+	public static void main(String[] args) {
+		GreensTech2 g = new GreensTech2();
+		g.greensAdyar();
+		g.greensOmr();
+		g.greensVelachery();
+		g.greensAnnaNagar();
+		g.greensTambaram();
+	}
+
+}
